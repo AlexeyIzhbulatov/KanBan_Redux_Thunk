c74e951fccd29acd7226b2e97e8600a910532ee4
AlexeyIzhbulatov <ouren@mail.ru>
2020-12-03 16:14:54 +0300

    install 1/3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
 
-function App() {
+function App(props) {
+  console.log(props.t)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
+const mapStateToProps = (state) => ({
+ t: state.todos
+})
 
-export default App;
+export default connect(mapStateToProps)(App);
